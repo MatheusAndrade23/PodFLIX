1abcf5d204182a48d739bde728261a85babd8bbc
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2021-12-13 20:56:45 -0300

    Primeiro JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var assistir = document.querySelector('#assistir');
+
+assistir.addEventListener("click", function () {
+
+    window.location.href = "https://www.youtube.com/watch?v=0rS25QzvAzQ&t=6265s";
+
+})
