edfed311b2063c78654b7e109d8001d72c07daae
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2021-12-14 21:50:59 -0300

    Animacoes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ assistir.addEventListener("click", function () {
     window.location.href = "https://www.youtube.com/watch?v=0rS25QzvAzQ&t=6265s";
 
 })
+
+var fright = document.querySelector('#fright');
+
+fright.addEventListener("click", function () {
+
+
+})
