5c0d18a851fefd2f2b0fca809d8df8c7330130fd
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2021-12-16 13:43:58 -0300

    .

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-var assistir = document.querySelector('#assistir');
-
-assistir.addEventListener("click", function () {
-
-    window.location.href = "https://www.youtube.com/watch?v=0rS25QzvAzQ&t=6265s";
-
-})
