a3021106be24cc88423c869c963f0335e5440734
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2021-12-16 20:09:00 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var assistir = document.querySelector('#assistir');
+
+        assistir.addEventListener("click", function () {
+
+            window.location.href = "https://www.youtube.com/watch?v=0rS25QzvAzQ&t=6265s";
+
+        })
+
+        var fbutton = document.querySelector('#fbutton');
+        var sbutton = document.querySelector('#sbutton');
+        var tbutton = document.querySelector('#tbutton');
+        var qbutton = document.querySelector('#qbutton');
+        var cbutton = document.querySelector('#cbutton');
+        var sebutton = document.querySelector('#sebutton');
+
+        fbutton.addEventListener("click", function () {
+
+
+        })
