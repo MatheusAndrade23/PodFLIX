2b7230ff37198055051380f3bd1cef4c7e070533
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2021-12-16 20:35:48 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,19 +1,42 @@
 var assistir = document.querySelector('#assistir');
 
-        assistir.addEventListener("click", function () {
+assistir.addEventListener("click", function () {
 
-            window.location.href = "https://www.youtube.com/watch?v=0rS25QzvAzQ&t=6265s";
+    window.location.href = "https://www.youtube.com/watch?v=0rS25QzvAzQ&t=6265s";
 
-        })
+})
 
-        var fbutton = document.querySelector('#fbutton');
-        var sbutton = document.querySelector('#sbutton');
-        var tbutton = document.querySelector('#tbutton');
-        var qbutton = document.querySelector('#qbutton');
-        var cbutton = document.querySelector('#cbutton');
-        var sebutton = document.querySelector('#sebutton');
+const fbutton = document.querySelector('#fbutton');
+var controle1 = 0;
 
-        fbutton.addEventListener("click", function () {
+const sbutton = document.querySelector('#sbutton');
+var controle2 = 0;
+
+const tbutton = document.querySelector('#tbutton');
+var controle3 = 0;
+
+const qbutton = document.querySelector('#qbutton');
+var controle4 = 0;
+
+const cbutton = document.querySelector('#cbutton');
+var controle5 = 0;
+
+const sebutton = document.querySelector('#sebutton');
+var controle6 = 0;
 
 
-        })
+fbutton.addEventListener("click", function () {
+
+    if(controle1 == 0){
+
+        document.querySelector('#hidden11').style.display = "flex";
+        document.querySelector('#hidden12').style.display = "flex";
+        controle1 = 1;
+
+    }else{
+
+        document.querySelector('#hidden11').style.display = "none";
+        document.querySelector('#hidden12').style.display = "none";
+        controle1 = 0;
+    }   
+})
